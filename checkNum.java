b49157a42b3ff74973b4129323de3cd559cb0b62
Mucyoenock <55673018+Mucyoenock@users.noreply.github.com>
2021-01-23 19:11:40 +0200

    Add files via upload

---
+++ checkNum.java
@@ -0,0 +1,23 @@
+class checkNum{
+public static void main(String[] args) {
+
+/**int i,j;
+
+for (i=1;i<10;i++)
+{
+	for (j=1;j<=i;j++)
+	{
+		System.out.print(i);
+	}System.out.println();
+}*/
+/**int n=1;
+while (n<=10)
+{
+	System.out.println(n);
+	n++;
+}*/
+ int n=1;
+ for (n=1;n<=10;n++){
+ 	System.out.println(n);}
+
+}}
